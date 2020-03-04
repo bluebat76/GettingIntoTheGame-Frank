4d7aa364cc08b1588ad27e9eef47ae3f455dbea9
CBTE Account <cbte@ac.sdmesa.net>
2020-03-04 12:27:58 -0800

    Lesson 2D resubmitted

---
+++ Assets/Course Library/Scrpits/NewPlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerController : MonoBehaviour
+{
+    public float xRange = 20;
+    public float speed = 10;
+    public GameObject ProjectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float HInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * HInput * speed * Time.deltaTime);
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(ProjectilePrefab, transform.position, ProjectilePrefab.transform.rotation);
+        }
+
+
+
+    }
+}
---
+++ Assets/Course Library/Scrpits/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] Animals;
+    private const float xRange = 10;
+    private const float SpawnPointZ = 25;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            int Randamals = Random.Range(0, Animals.Length - 1);
+            Instantiate(Animals[Randamals], new Vector3(Random.Range(-xRange, xRange), 0, SpawnPointZ), Animals[Randamals].transform.rotation);
+        }    
+    }
+}
